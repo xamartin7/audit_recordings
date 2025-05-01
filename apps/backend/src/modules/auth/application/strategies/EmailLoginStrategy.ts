8686fa021f273177ea171c31901e37df4d72a06d
xamartin7 <xavimartinalonso@gmail.com>
2025-05-01 17:45:21 +0200

    Add EmailLoginStrategy constructor and create new cookie file

--- apps/backend/src/modules/auth/application/strategies/EmailLoginStrategy.ts
+++ apps/backend/src/modules/auth/application/strategies/EmailLoginStrategy.ts
@@ -3,6 +3,8 @@ import { User } from "../../domain/User";
 import { LoginStrategy } from "./LoginStrategy";
 
 export class EmailLoginStrategy implements LoginStrategy {
+    public constructor() {}
+    
     public async login(credentials: {email: string, password: string}): Promise<{user: User, authToken: AuthToken}> {
         throw new Error('Not implemented');
     }

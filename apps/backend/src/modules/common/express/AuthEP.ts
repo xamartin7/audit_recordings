423f457c0d619b11c21670477ca3b733c95f95dc
xamartin7 <xavimartinalonso@gmail.com>
2025-05-01 18:14:03 +0200

    Backend auth system working

--- apps/backend/src/modules/common/express/AuthEP.ts
+++ apps/backend/src/modules/common/express/AuthEP.ts
@@ -1,8 +1,8 @@
 import { Express, Request, Response } from "express";
-import { EmailLoginStrategy } from "src/modules/auth/application/strategies/EmailLoginStrategy";
-import { LoginWithEmailUseCase } from "src/modules/auth/application/use-cases/LoginWithEmailUseCase";
-import { LoginWithGoogleUseCase } from "src/modules/auth/application/use-cases/LoginWithGoogleUseCase";
-import { AuthController } from "src/modules/auth/infrastructure/controllers/Auth.controller";
+import { EmailLoginStrategy } from "../../auth/application/strategies/EmailLoginStrategy";
+import { LoginWithEmailUseCase } from "../../auth/application/use-cases/LoginWithEmailUseCase";
+import { LoginWithGoogleUseCase } from "../../auth/application/use-cases/LoginWithGoogleUseCase";
+import { AuthController } from "../../auth/infrastructure/controllers/Auth.controller";
 
 export const setupAuthEP = (app: Express) => {
     const authController = new AuthController(new LoginWithEmailUseCase(new EmailLoginStrategy()), new LoginWithGoogleUseCase());

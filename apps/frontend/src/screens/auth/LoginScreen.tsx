3d858fedfc63317492971426581541d1e644364e
xamartin7 <xavimartinalonso@gmail.com>
2025-05-04 11:35:26 +0200

    Mui styles

--- apps/frontend/src/screens/auth/LoginScreen.tsx
+++ apps/frontend/src/screens/auth/LoginScreen.tsx
@@ -1,13 +1,13 @@
 import { AuthLayout } from '../../layouts/AuthLayout';
 import { LoginForm } from '../../components/LoginForm';
 import { useState } from 'react';
-
+import { Alert } from '@mui/material';
 export function LoginScreen() {
     const [error, setError] = useState<string>('');
     return (
         <AuthLayout>
             <h1 className="text-2xl font-bold text-center text-gray-900">Login</h1>
-            {error && <p className="text-red-500 text-center">{error}</p>}
+            {error && <Alert severity="error">{error}</Alert>}
             <LoginForm setError={setError}/>
         </AuthLayout>
     );

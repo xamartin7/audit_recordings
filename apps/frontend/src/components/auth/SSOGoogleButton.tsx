41bba6e2178d4333f91083a506d171ae0747b4b0
xamartin7 <xavimartinalonso@gmail.com>
2025-05-09 23:22:35 +0200

    Functions to manage the session after sso with google V1 (Not wotking)

--- apps/frontend/src/components/auth/SSOGoogleButton.tsx
+++ apps/frontend/src/components/auth/SSOGoogleButton.tsx
@@ -4,6 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 import { EnvConfig } from '../../utils/EnvConfig';
 import { Button } from '@mui/material';
 import { FaGoogle } from "react-icons/fa";
+import { useEffect } from 'react';
 
 const supabase = createClient(
   EnvConfig.getEnvVariables().supabaseUrl,
@@ -12,11 +13,38 @@ const supabase = createClient(
 
 export function SSOGoogleButton() {
 
-    function handleLogin() {
+    useEffect(() => {
+        // Watch for auth state changes
+        const {
+            data: { subscription },
+        } = supabase.auth.onAuthStateChange((event, session) => {
+            console.log('Auth state change:', event)
+            if (session) {
+                console.log('Session is ready:', session)
+                //setAccessToken(session.access_token) // <-- this is your backend token
+            }
+        })
+
+        // Initial fetch after hydration
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            if (session) {
+                // setAccessToken(session.access_token)
+                console.log('Initial session restored:', session)
+            } else {
+                console.log('No session on initial load')
+            }
+        })
+
+        return () => subscription.unsubscribe()
+    }, [])
+
+    function handleLogin(e: React.MouseEvent<HTMLButtonElement>) {
+        e.preventDefault()
         supabase.auth.signInWithOAuth({
             provider: 'google',
             /*options: {
-                redirectTo: 'http://localhost:3000/auth/callback',
+                //redirectTo: 'http://localhost:3000/auth/callback',
+                redirectTo: window.location.origin,
             },*/
         })
     }

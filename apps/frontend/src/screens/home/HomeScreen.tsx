6dba626607d31ed5fd21628c8801b17a2de782cd
xamartin7 <xavimartinalonso@gmail.com>
2025-06-14 17:16:50 +0200

    Styles layouts for the main contents

--- apps/frontend/src/screens/home/HomeScreen.tsx
+++ apps/frontend/src/screens/home/HomeScreen.tsx
@@ -1,24 +1,10 @@
-import { Sidebar } from '../../components/common/Sidebar';
-//import { Topnav } from '../../components/common/Topnav';
+import { MainContentSidebarLayout } from '../../layouts/MainContentSidebarLayout';
 
 export function HomeScreen() {
 
     return (
-        <div className="flex h-screen bg-gray-100">
-            {/* Sidebar */}
-            <Sidebar></Sidebar>
-
-            {/* Main content */}
-            <div className="flex-1 flex flex-col">
-                {/* Top navigation */}
-                {/* <Topnav></Topnav> */}
-
-                {/* Content area */}
-                <div className="flex-1 p-6">
-                    <h2 className="text-2xl font-bold text-gray-900">Home Page</h2>
-                    <p className="text-gray-500">This is the Home page content.</p>
-                </div>
-            </div>
-        </div>
+        <MainContentSidebarLayout title="Home">
+            <p className="text-gray-500">This is the Home page content.</p>
+        </MainContentSidebarLayout>
     );
 }
